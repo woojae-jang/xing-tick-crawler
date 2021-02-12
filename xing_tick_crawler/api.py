518c08bbc22287de98e3c67229d884ed9814c289
quantrading <dnwogo@naver.com>
2021-02-13 07:56:53 +0900

    TR 추가  - t1702 외인/기관 종목별동향

--- xing_tick_crawler/api.py
+++ xing_tick_crawler/api.py
@@ -110,6 +110,111 @@ class XingAPI:
         return df
 
     @classmethod
+    def t1702(cls, code, start_date: str, end_date: str) -> pd.DataFrame:
+        """
+        t1702 외인/기관 종목별동향
+        연속데이터 조회
+
+        start_date: YYYYMMDD
+        end_date: YYYYMMDD
+        """
+        stock_code = code
+        t1702 = win32com.client.DispatchWithEvents("XA_DataSet.XAQuery", XAQueryEventHandler)
+        t1702.ResFileName = f"{RES_FOLDER_PATH}/t1702.res"
+
+        # 연속조회 여부
+        next_data = False
+        data = []
+
+        while True:
+            t1702.SetFieldData('t1702InBlock', 'shcode', 0, stock_code)
+            t1702.SetFieldData('t1702InBlock', 'todt', 0, end_date)
+            t1702.SetFieldData('t1702InBlock', 'volvalgb', 0, '1')  # 0: 금액, 1: 수량, 2: 단가
+            t1702.SetFieldData('t1702InBlock', 'msmdgb', 0, '0')  # 0: 순매수, 1: 매수, 2: 매도
+            t1702.SetFieldData('t1702InBlock', 'cumulgb', 0, '0')  # 0:일간, 1: 누적
+            if next_data is False:
+                t1702.SetFieldData('t1702InBlock', 'cts_date', 0, '')
+                t1702.SetFieldData('t1702InBlock', 'cts_idx', 0, '')
+            else:
+                t1702.SetFieldData('t1702InBlock', 'cts_date', 0, cts_date)
+                t1702.SetFieldData('t1702InBlock', 'cts_idx', 0, cts_idx)
+
+            if next_data:
+                cls.request_api(t1702, 1)
+            else:
+                cls.request_api(t1702, 0)
+            cls.wait_query(XAQueryEventHandler)
+
+            cts_idx = t1702.GetFieldData("t1702OutBlock", "cts_idx", 0)
+            cts_date = t1702.GetFieldData("t1702OutBlock", "cts_date", 0)
+            next_data = True
+
+            if cts_date < start_date:
+                break
+
+            count = t1702.GetBlockCount("t1702OutBlock1")
+            if count == 0:
+                break
+            for i in range(count):
+                date = t1702.GetFieldData("t1702OutBlock1", "date", i)
+                amt0000 = t1702.GetFieldData("t1702OutBlock1", "amt0000", i)  # 사모펀드
+                amt0001 = t1702.GetFieldData("t1702OutBlock1", "amt0001", i)  # 증권
+                amt0002 = t1702.GetFieldData("t1702OutBlock1", "amt0002", i)  # 보험
+                amt0003 = t1702.GetFieldData("t1702OutBlock1", "amt0003", i)  # 투신
+                amt0004 = t1702.GetFieldData("t1702OutBlock1", "amt0004", i)  # 은행
+                amt0005 = t1702.GetFieldData("t1702OutBlock1", "amt0005", i)  # 종금
+                amt0006 = t1702.GetFieldData("t1702OutBlock1", "amt0006", i)  # 기금
+                amt0007 = t1702.GetFieldData("t1702OutBlock1", "amt0007", i)  # 기타법인
+                amt0008 = t1702.GetFieldData("t1702OutBlock1", "amt0008", i)  # 개인
+                amt0009 = t1702.GetFieldData("t1702OutBlock1", "amt0009", i)  # 등록외국인
+                amt0010 = t1702.GetFieldData("t1702OutBlock1", "amt0010", i)  # 미등록외국인
+                amt0011 = t1702.GetFieldData("t1702OutBlock1", "amt0011", i)  # 국가외
+                amt0018 = t1702.GetFieldData("t1702OutBlock1", "amt0018", i)  # 기관
+                amt0088 = t1702.GetFieldData("t1702OutBlock1", "amt0088", i)  # 외인(등록외국인 + 미등록외국인)
+                amt0099 = t1702.GetFieldData("t1702OutBlock1", "amt0099", i)  # 기타계(기타 + 국가)
+
+                data.append([
+                    date,
+                    amt0000,
+                    amt0001,
+                    amt0002,
+                    amt0003,
+                    amt0004,
+                    amt0005,
+                    amt0006,
+                    amt0007,
+                    amt0008,
+                    amt0009,
+                    amt0010,
+                    amt0011,
+                    amt0018,
+                    amt0088,
+                    amt0099,
+                ])
+                print(date)
+
+        df = pd.DataFrame(data, columns={
+            "date": str,
+            "사모펀드": str,
+            "증권": str,
+            "보험": str,
+            "투신": str,
+            "은행": str,
+            "종금": str,
+            "기금": str,
+            "기타법인": str,
+            "개인": str,
+            "등록외국인": str,
+            "미등록외국인": str,
+            "국가외": str,
+            "기관": str,
+            "외인": str,
+            "기타계": str,
+        })
+        df = df.sort_values('date', ascending=True)
+        return df
+
+    @classmethod
     def get_stock_futures_listed_code_list(cls) -> pd.DataFrame:
         """
         [t8401] 주식선물 마스터조회
